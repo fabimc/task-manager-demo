f05af57d730ddb0b9f25913e65e1c91939f7cc90
Fabián Montealegre <fabimc@gmail.com>
2024-01-11 22:00:29 -0600

    adds Bulma css framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { TaskComponent } from './pages/task/task.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
