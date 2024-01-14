0436ee46200010296c8e4b26a10afb8acdf8abdd
Fabián Montealegre <fabimc@gmail.com>
2024-01-13 21:15:07 -0600

    deletes and completes

--- frontend/src/app/pages/tasks/task-item/task-item.component.ts
+++ frontend/src/app/pages/tasks/task-item/task-item.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Task } from '../../../core/interfaces/task.interface';
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.scss'
 })
 export class TaskItemComponent {
+  @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onCompleteTask: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onComplete(task: Task) {
+    this.onCompleteTask.emit(task);
+  }
 }
--- frontend/src/app/pages/tasks/tasks.component.ts
+++ frontend/src/app/pages/tasks/tasks.component.ts
@@ -1,22 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TaskService } from '../../core/services/tasks/task.service';
 import { Task } from '../../core/interfaces/task.interface';
+import { Observable, catchError, filter, map, of } from 'rxjs';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss'
+  styleUrl: './tasks.component.scss',
+  imports: [CommonModule, TaskItemComponent],
 })
 export class TasksComponent {
+  tasks$: Observable<Task[]>;
 
-  //tasks: Task[] = [];
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService) {
+    this.tasks$ = new Observable<Task[]>();
+  }
 
   ngOnInit(): void {
-    this.taskService.getTasks().subscribe((tasks) => {
-      console.log(tasks);
+    this.tasks$ = this.taskService
+      .getTasks()
+      .pipe(catchError((error) => [error]));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task.id).subscribe(() => {
+      this.tasks$ = this.tasks$.pipe(
+        map((tasks) => tasks.filter((t) => t.id !== task.id))
+      );
+    });
+  }
+
+  completeTask(task: Task) {
+    this.taskService.updateTask({ ...task, completed: !task.completed}).subscribe(() => {
+      this.tasks$ = this.tasks$.pipe(
+        map((tasks) => tasks.map((t) => t.id === task.id ? { ...task, completed: !task.completed } : t)
+      ));
     });
   }
 
